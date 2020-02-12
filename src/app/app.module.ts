3cc90db2e4d67b20b6f427b99ce511a6ddc0edff
Hugo Peres <hugo-pdf@hotmail.com>
2020-02-12 16:05:17 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { VariantsComponent } from './components/variants/variants.component';
+import { ChassisComponent } from './components/chassis/chassis.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    VariantsComponent,
+    ChassisComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/chassis/chassis.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chassis',
+  templateUrl: './chassis.component.html',
+  styleUrls: ['./chassis.component.css']
+})
+export class ChassisComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
